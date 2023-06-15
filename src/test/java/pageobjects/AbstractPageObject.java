290f3918e81e40a73d60654138aa61c1085a5106
Vika Kolomiets <vikakolomiets@gmail.com>
2023-06-15 21:23:33 +0300

    created AbstractPageObject

---
+++ src/test/java/pageobjects/AbstractPageObject.java
@@ -0,0 +1,22 @@
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class AbstractPageObject {
+
+    private WebDriver driver;
+
+    public AbstractPageObject(WebDriver driver) {
+        this.driver = driver;
+    }
+    public WebElement getWebElement(By by){
+        return new WebDriverWait(this.driver, Duration.ofSeconds(25) )
+                .until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+}
