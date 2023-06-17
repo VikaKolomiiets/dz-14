c3a8df428d25993733025a3a0b3143557700c629
Vika Kolomiets <vikakolomiets@gmail.com>
2023-06-17 22:55:44 +0300

    added method ButtonsPage

--- src/main/java/pages/ButtonsPage.java
+++ src/main/java/pages/ButtonsPage.java
@@ -1,9 +1,21 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ButtonsPage extends AbstractPage{
+
+    private By clickMeButton = By.cssSelector("#yXuZR");
+    private By message = By.cssSelector("#dynamicClickMessage");
+
     public ButtonsPage(WebDriver driver) {
         super(driver);
     }
+
+    public String getTestAfterClickOnButtonClickMe(){
+        this.findElementVisibleWithFluentWait(clickMeButton).click();
+        return this.findElementVisibleWithFluentWait(message).getText();
+    }
+
+
 }
