a5feba81180eb5cc1bb52101e1927fc98f981579
Vika Kolomiets <vikakolomiets@gmail.com>
2023-06-17 21:34:21 +0300

    added methods with Fluent wait

--- src/main/java/AbstractPage.java
+++ src/main/java/AbstractPage.java
@@ -1,4 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public abstract class AbstractPage {
     private WebDriver driver;
@@ -10,4 +18,24 @@ public abstract class AbstractPage {
     protected WebDriver getDriver() {
         return driver;
     }
-}
+
+    public WebElement findElementVisibleWithFluentWait(By by){
+        FluentWait fluentWait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(6))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(Exception.class);
+
+        fluentWait.until(ExpectedConditions.invisibilityOf(this.driver.findElement(by)));
+        return this.driver.findElement(by);
+    }
+
+    public List<WebElement> findElementsVisibleWithFluentWait(By by){
+        FluentWait fluentWait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(6))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(Exception.class);
+
+        fluentWait.until(ExpectedConditions.invisibilityOf(this.driver.findElement(by)));
+        return this.driver.findElements(by);
+    }
+ }
