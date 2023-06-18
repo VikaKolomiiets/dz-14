3b1c8ae35bf6426b67be7025c99cc4bdc9c3e006
Vika Kolomiets <vikakolomiets@gmail.com>
2023-06-18 21:11:46 +0300

    added method to WebTablesPage

--- src/main/java/pages/WebTablesPage.java
+++ src/main/java/pages/WebTablesPage.java
@@ -16,6 +16,7 @@ public class WebTablesPage extends AbstractPage {
     private By salaryBy = By.id("salary");
     private By departmentBy = By.id("department");
     private By submitButton = By.cssSelector("#submit");
+    private By closeButton = By.cssSelector(".close");
 
     public WebTablesPage(WebDriver driver) {
         super(driver);
@@ -28,59 +29,70 @@ public class WebTablesPage extends AbstractPage {
         return title.getText();
     }
 
-    public void setAllDataInRegistrationForm(){
-        
+    public void setAllDataInRegistrationForm(String firstName, String lastName, String email, Integer age, Double salary, String department ){
+        this.clickOnAddButton();
+        this.setFirstNameInModalWindow(firstName);
+        this.setLastNameInModalWindow(lastName);
+        this.setUserEmailInModalWindow(email);
+        this.setAgeInModalWindow(age);
+        this.setSalaryInModalWindow(salary);
+        this.setDepartmentInModalWindow(department);
+        this.clickOnSubmitButton();
     }
 
 
-    public void setFirstName(String firstName){
-        this.clickOnAddButton();
+    public void setFirstNameInModalWindow(String firstName){
         WebElement firstNameElement = this.findElementVisibleWithFluentWait(firstNameBy);
         firstNameElement.sendKeys(firstName);
     }
 
-    public void setLastName(String lastName){
-        this.clickOnAddButton();
+    public void setLastNameInModalWindow(String lastName){
         WebElement lastNameElement = this.findElementVisibleWithFluentWait(lastNameBy);
         lastNameElement.sendKeys(lastName);
     }
 
-    public void setUserEmail(String email){
-        this.clickOnAddButton();
+    public void setUserEmailInModalWindow(String email){
+        if(!email.contains("@")){
+            throw new IllegalArgumentException(String.format("%s is not correct for e-mail, please check for @.", email));
+        }
         WebElement emailElement = this.findElementVisibleWithFluentWait(userEmailBy);
         emailElement.sendKeys(email);
     }
 
-    public void setAge(String age){
-        this.clickOnAddButton();
-        WebElement agelement = this.findElementVisibleWithFluentWait(ageBy);
-        agelement.sendKeys(age);
+    public void setAgeInModalWindow(Integer age){
+        if(age < 14){
+            throw new IllegalArgumentException(String.format("$d is not acceptable age for employee for this place", age ));
+        }
+        WebElement ageElement = this.findElementVisibleWithFluentWait(ageBy);
+        ageElement.sendKeys(age.toString());
     }
-    public void setSalary(String salary){
-        this.clickOnAddButton();
+    public void setSalaryInModalWindow(Double salary){
+        if(salary < 500.00){
+            throw new IllegalArgumentException(String.format("$d should be more than 500.00 or more", salary ));
+        }
         WebElement salaryElement = this.findElementVisibleWithFluentWait(salaryBy);
-        salaryElement.sendKeys(salary);
+        salaryElement.sendKeys(salary.toString());
     }
 
-    public void setDepartment(String department){
-        this.clickOnAddButton();
+    public void setDepartmentInModalWindow(String department){
+        if(department == null || department.isEmpty()){
+            throw new NullPointerException("Please, fill in the Department");
+        }
+        if(department.length() < 2){
+            throw new IllegalArgumentException("Please, check Department name: it should contain more than 1 letter");
+        }
         WebElement departmentElement = this.findElementVisibleWithFluentWait(departmentBy);
         departmentElement.sendKeys(department);
     }
 
-
-
-
-
-
-
-
-
-    private void clickOnAddButton(){
+    public void clickOnAddButton(){
         this.findElementVisibleWithFluentWait(addButton).click();
     }
-
-   
-
+    public void clickOnSubmitButton(){
+        this.findElementVisibleWithFluentWait(submitButton);
+    }
+    public void clickOnCloseButton(){
+        this.findElementVisibleWithFluentWait(closeButton);
+    }
 
 }
